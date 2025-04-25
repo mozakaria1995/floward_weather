e031048b26efc49c84d5559fc9abec11f2e423c1
Mohamed Zakaria <86918314+mozakaria1995@users.noreply.github.com>
2025-04-25 16:21:02 +0300

    Add internet permission to AndroidManifest and enhance Failure classes with detailed error messages

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -7,17 +7,25 @@ abstract class Failure extends Equatable {
   const Failure([this.message = '', this.properties = const <dynamic>[]]);
 
   @override
-  List<Object> get props => [properties];
+  List<Object> get props => [message, properties];
+
+  @override
+  String toString() {
+    return message.isNotEmpty ? message : runtimeType.toString();
+  }
 }
 
 class ServerFailure extends Failure {
-  const ServerFailure([String message = '']) : super(message);
+  const ServerFailure([String message = 'Server error occurred'])
+      : super(message);
 }
 
 class CacheFailure extends Failure {
-  const CacheFailure([String message = '']) : super(message);
+  const CacheFailure([String message = 'Failed to retrieve cached data'])
+      : super(message);
 }
 
 class NetworkFailure extends Failure {
-  const NetworkFailure([String message = '']) : super(message);
+  const NetworkFailure([String message = 'No internet connection'])
+      : super(message);
 }
